f988289a2f181140fc4487f00e5c94d0aa4cb49b
Kumar Aditya <workmail7301@gmail.com>
2024-04-11 13:28:37 +0530

    Add user avatar and cover image update endpoints to user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -282,6 +282,70 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Account details updated successfully"));
 });
 
+//code for updating user avatar----
+const updateUserAvatar = asyncHandler(async (res, req) => {
+  const avatarLocalPath = req.file?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is missing");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar.url) {
+    throw new ApiError(400, "Error while uploading  avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar updated successfully"));
+});
+
+//code for updating user cover image----
+const updateUserCoverImage = asyncHandler(async (res, req) => {
+  const coverImageLocalPath = req.file?.path;
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "Cover Image file is missing");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new ApiError(400, "Error while uploading CoverImage");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image updated successfully"));
+});
+
+
+
 //exporting the functions to be used in other files-----
 export {
   registerUser,
@@ -291,4 +355,6 @@ export {
   getCurrentUser,
   updateAccountDetails,
   chnageCurrentPassword,
+  updateUserAvatar,
+  updateUserCoverImage
 };
