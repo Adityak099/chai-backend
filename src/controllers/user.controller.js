a032203293932719a573922988a3ba8bc977b80b
Kumar Aditya <workmail7301@gmail.com>
2024-04-11 19:30:56 +0530

    Add getUserChannelProfile endpoint to user.controller.js and refreshAccessToken endpoint to user.routes.js. Further added some aggregation pipelines for getting user channel profile.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -344,7 +344,73 @@ const updateUserCoverImage = asyncHandler(async (res, req) => {
     .json(new ApiResponse(200, user, "Cover Image updated successfully"));
 });
 
+//code for getting user channel profile----
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
 
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscription",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscription",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: { $size: "$subscribers" },
+        channelsSubscribedToCount: { $size: "$subscribedTo" },
+      },
+      isSubscribed: {
+        $cond: {
+          if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+          then: true,
+          else: false,
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+    // console.log(channel),
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
 
 //exporting the functions to be used in other files-----
 export {
@@ -356,5 +422,6 @@ export {
   updateAccountDetails,
   chnageCurrentPassword,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile,
 };
